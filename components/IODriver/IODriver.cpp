2b7fc47fd961310466b951d5917ffcbdd8780d13
Nicolas <nico645@outlook.com>
2024-02-28 21:14:02 -0500

    Add update to attribute when the intensity change

--- components/IODriver/IODriver.cpp
+++ components/IODriver/IODriver.cpp
@@ -27,14 +27,34 @@ esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_
 {
     // if the cluster is for on/off switch
     if (endpoint_id == 1) {
+        // Get the node id of the device
+        //uint64_t node_id = 0;
+
         if (cluster_id == OnOff::Id) {
             if (attribute_id == OnOff::Attributes::OnOff::Id) {
-                 // manage the on/off switch
+                ESP_LOGI(TAG, "OnOff: %d", val->val.b);
+                // log the val type
+                ESP_LOGI(TAG, "Val type: %d", (int)val->type);
             }
         }
         else if (cluster_id == LevelControl::Id) {
             if (attribute_id == LevelControl::Attributes::CurrentLevel::Id) {
-                // manage the dimmer switch
+
+                ESP_LOGI(TAG, "Current level: %d", val->val.u16);
+
+
+                if (val->val.u16 == 0) {
+                    esp_matter_attr_val_t attr_val;
+                    attr_val.type = (esp_matter_val_type_t)1;
+                    attr_val.val.b = false;
+                    esp_matter::attribute::update(endpoint_id, OnOff::Id, OnOff::Attributes::OnOff::Id, &attr_val);
+                }
+                else {
+                    esp_matter_attr_val_t attr_val;
+                    attr_val.val.b = true;
+                    attr_val.type = (esp_matter_val_type_t)1;
+                    esp_matter::attribute::update(endpoint_id, OnOff::Id, OnOff::Attributes::OnOff::Id, &attr_val);
+                }
             }
         }
     }
--- components/SwitchConfigurator/SwitchConfigurator.cpp
+++ components/SwitchConfigurator/SwitchConfigurator.cpp
@@ -12,8 +12,8 @@ endpoint_t *configureOnOffSwitch(on_off_switch::config_t &c_switch, uint8_t flag
 endpoint_t *configureDimmableSwitch(dimmable_plugin_unit::config_t &s_switch, dimmer_switch::config_t &c_switch, uint8_t flags, void *priv_data, esp_matter::node_t *node)
 {
     //priv_endpoint = dimmer_switch::create(node, &c_switch, flags, priv_data);
-    c_switch.on_off.on_off = false;
-    c_switch.level_control.on_level = (uint8_t)0;
+    s_switch.on_off.on_off = false;
+    s_switch.level_control.current_level = (uint8_t)0;
     priv_endpoint = dimmable_plugin_unit::create(node, &s_switch, flags, priv_data);
 
     // add switch client cluster
